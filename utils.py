845389a263f9c7c554dd77b8aee9a99c7bec0340
qshadun <qshadun@gmail.com>
2024-05-31 22:50:46 +0800

    day 1

--- utils.py
+++ utils.py
@@ -3,3 +3,17 @@ def read_lines(input_file):
     for line in open(input_file):
         ans.append(line.rstrip('\n'))
     return ans
+
+left_turn = {
+    (-1, 0): (0, -1),
+    (1, 0): (0, 1),
+    (0, -1): (1, 0),
+    (0, 1): (-1, 0),
+}
+
+right_turn = {
+    (-1, 0): (0, 1),
+    (1, 0): (0, -1),
+    (0, -1): (-1, 0),
+    (0, 1): (1, 0),
+}
